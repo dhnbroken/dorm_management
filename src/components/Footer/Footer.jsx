6ad27a3ffe0082476d5a79a9821bef96da59574b
dhnbroken <bdfnatsu@gmail.com>
2023-11-23 16:15:56 +0700

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import './footer.css';
+const Footer = () => {
+  return (
+    <Box className="footer">
+      <Typography component="p">Just for KLTN</Typography>
+    </Box>
+  );
+};
+
+export default Footer;
