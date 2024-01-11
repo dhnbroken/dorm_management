d9bf00055c8f1216143495415522c4bd0a585a83
dhnbroken <bdfnatsu@gmail.com>
2024-01-11 10:09:16 +0700

    feat: add create bill for admin

--- src/pages/Admin/BillListPage.jsx
+++ src/pages/Admin/BillListPage.jsx
@@ -1,9 +1,12 @@
+import { PlusIcon } from '@heroicons/react/20/solid';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { getAllBills, updateBill } from 'API/bill';
-import { getAllRooms } from 'API/room';
-import { Modal, Space } from 'antd';
+import { createBill, getAllBills, updateBill } from 'API/bill';
+
+import { getAllStudent } from 'API/user';
+import { Modal, Select, Space } from 'antd';
 import { PrimaryButton } from 'components/Button/PrimaryButton';
 import CustomTable from 'components/CustomTable';
+import DatePicker from 'components/DatePicker';
 import InputWithLabel from 'components/Input/InputWithLabel';
 import EditIcon from 'components/icons/EditIcon';
 import moment from 'moment';
@@ -19,15 +22,32 @@ const BillListPage = () => {
     queryFn: getAllBills
   });
 
+  const { data: allUser } = useQuery({
+    queryKey: ['get_all_user'],
+    queryFn: getAllStudent
+  });
+
   const [isModalEdit, setIsModalEdit] = useState(false);
   const [editData, setEditData] = useState({});
+  const [newBillModal, setNewBillModal] = useState(false);
+  const [userOptions, setUserOptions] = useState([]);
+  const [selectedUser, setSelectedUser] = useState();
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset
-  } = useForm();
+  const [dateIn, setDateIn] = useState();
+  const [dateOut, setDateOut] = useState();
+
+  useEffect(() => {
+    if (allUser) {
+      const newArray = allUser?.map((user) => ({
+        value: JSON.stringify(user),
+        label: user.HoTen
+      }));
+
+      setUserOptions(newArray);
+    }
+  }, [allUser]);
+
+  const { register, handleSubmit, reset } = useForm();
 
   const columns = [
     {
@@ -57,6 +77,13 @@ const BillListPage = () => {
       }
     },
     {
+      title: 'Ngày đến hạn',
+      dataIndex: 'billDetails',
+      render: (_, record) => {
+        return <div className="text-center">{moment(record?.billDetails?.dateOut).format('DD/MM/YYYY')}</div>;
+      }
+    },
+    {
       title: 'Thời gian cập nhật',
       dataIndex: 'updatedAt',
       render: (_, record) => {
@@ -82,6 +109,8 @@ const BillListPage = () => {
     }
   ];
 
+  console.log(allBills);
+
   const updateBillData = useMutation({
     mutationFn: updateBill,
     onSuccess: () => {
@@ -90,12 +119,28 @@ const BillListPage = () => {
       queryClient.invalidateQueries({ queryKey: ['all_bills'] });
       queryClient.invalidateQueries({ queryKey: ['all_rooms'] });
 
+      setSelectedUser({});
+      setDateIn('');
+      setDateOut('');
       reset();
     },
     onError: () => {
       toast.error('Có lỗi xảy ra, xin thử lại!');
     }
   });
+
+  const handlePaid = () => {
+    updateBillData.mutate({
+      id: editData?._id,
+      data: {
+        status: 1
+      }
+    });
+  };
+
+  const handleChangeUser = (value) => {
+    setSelectedUser(JSON.parse(value));
+  };
 
   const onSubmit = (data) => {
     updateBillData.mutate({
@@ -111,17 +156,42 @@ const BillListPage = () => {
     });
   };
 
-  const handlePaid = () => {
-    updateBillData.mutate({
-      id: editData?._id,
-      data: {
-        status: 1
-      }
+  const handleCreateBill = useMutation({
+    mutationFn: createBill,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['all_bills'] });
+      queryClient.invalidateQueries({ queryKey: ['get_all_user'] });
+
+      toast.success('Tạo thành công');
+      setNewBillModal(false);
+      reset();
+    }
+  });
+
+  const onCreateBill = (formData) => {
+    const data = {
+      ...formData,
+      dateIn: moment(dateIn, 'DD/MM/YYYY').toDate(),
+      dateOut: moment(dateOut, 'DD/MM/YYYY').toDate(),
+      roomId: selectedUser?.room?.roomId,
+      status: 0,
+      CMND: selectedUser?.CMND,
+      userId: selectedUser?._id,
+      Mssv: selectedUser?.Mssv,
+      roomName: selectedUser?.room?.roomTitle
+    };
+
+    handleCreateBill.mutate({
+      data
     });
   };
+
   return (
     <div className="p-8">
       <div className="text-xl font-semibold mb-8">Danh sách hóa đơn</div>
+      <div className="text-end mb-2">
+        <PrimaryButton text={'Tạo hóa đơn'} Icon={PlusIcon} onClick={() => setNewBillModal(true)} />
+      </div>
       <CustomTable columns={columns} dataSource={allBills} isPagination={false} />
       <Modal open={isModalEdit} onCancel={() => setIsModalEdit(false)} footer={false}>
         <div className="text-center w-full text-xl font-semibold my-3">Sửa thông tin hóa đơn</div>
@@ -153,6 +223,47 @@ const BillListPage = () => {
                 />
               )}
             </div>
+            <div className="sm:flex sm:flex-row-reverse">
+              <button
+                type="submit"
+                className="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:ml-3 sm:w-auto
+                disabled:bg-red-200 disabled:hover:bg-red-200"
+              >
+                Xác nhận
+              </button>
+              <button
+                type="button"
+                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                onClick={() => setIsModalEdit(false)}
+              >
+                Hủy
+              </button>
+            </div>
+          </div>
+        </form>
+      </Modal>
+
+      <Modal open={newBillModal} onCancel={() => setNewBillModal(false)} footer={false}>
+        <form className="flex justify-center flex-col gap-3" onSubmit={handleSubmit(onCreateBill)}>
+          <div className="flex flex-col gap-3">
+            <InputWithLabel label={'Tên hóa đơn'} register={register} registerKey={'title'} />
+            <div>
+              <div>Sinh viên</div>
+              <Select onChange={handleChangeUser} options={userOptions} className="w-full" />
+            </div>
+            <InputWithLabel label={'Giá'} register={register} registerKey={'price'} />
+            {/* Date In */}
+            <div>
+              <label>Ngày bắt đầu</label>
+              <DatePicker className={'!max-w-full'} setSelectedDate={setDateIn} />
+            </div>
+            {/* DateOut */}
+            <div>
+              <label>Ngày kết thúc</label>
+              <DatePicker className={'!max-w-full'} setSelectedDate={setDateOut} />
+            </div>
+          </div>
+          <div className="mt-5 sm:mt-4 flex justify-between">
             <div className="sm:flex sm:flex-row-reverse">
               <button
                 type="submit"
